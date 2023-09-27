c1a01f1fdddd4101d776b94ff6d252222ec41879
Nandu <Nandu@DESKTOP-9T6TKJU>
2023-09-27 15:21:06 -0600

    initial push

---
+++ nandu_demo_2709/src/main/java/com/example/demo/NanduDemo2709Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanduDemo2709Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanduDemo2709Application.class, args);
+	}
+
+}
